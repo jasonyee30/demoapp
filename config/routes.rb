2eddcd4609baf8ccb4e1f651e0a758ef682b853d
Jason Yee <j_yee@mba.berkeley.edu>
2016-09-23 00:23:57 +0000

    Added a new home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'status_pages/home'
+
+  get 'status_pages/help'
+
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
